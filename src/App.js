5ada9257f8f1a9e15ca2ef0f0b2a9761a4942b8b
Adilson Viegas <adilsonviegas@hotmail.com>
2021-07-05 10:38:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import React from 'react'
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  //const { reverse } = this.state;
+  const [reverse, setReverse] = useState(false);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    //this.setState({ reverse: !reverse });
+    setReverse(!reverse);
+    setCounter(counter + 1)
+  };
+
+  const handleIncrement = () =>{
+    setCounter(counter - 1)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+
+        <h1>Contador: {counter}</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <button type="button" onClick={handleClick}>
+          {' '}
+          Reverse {reverseClass}
+        </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
+/* class App extends Component{
+    state = {
+      reverse: true,
+    };
+  
+  handleClick = () => {
+  const { reverse } = this.state;
+  this.setState({ reverse: !reverse });
+}
+
+  render(){
+    const { reverse } = this.state;
+    const reverseClass = reverse ? 'reverse' : '';
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+          <button type="button" onClick={this.handleClick}>
+            {' '}
+            Reverse {reverseClass}
+          </button>
+        </header>
+      </div>
+    );
+  }
+} */
 export default App;
